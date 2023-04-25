483613c728690fbbdd46f3b391623ad85b08746a
Evgeniy Shakhov <79115839983@ya.ru>
2023-04-26 02:43:58 +0300

    lesson 3

--- app/src/main/java/com/eshakhov/jetpack1/MainActivity.kt
+++ app/src/main/java/com/eshakhov/jetpack1/MainActivity.kt
@@ -3,56 +3,78 @@ package com.eshakhov.jetpack1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.eshakhov.jetpack1.ui.theme.JetpackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column() {
+                ListItem(name = "Zac Efron", prof = "Actor")
+                ListItem(name = "Zac Efron", prof = "Actor")
+                ListItem(name = "Zac Efron", prof = "Actor")
+                ListItem(name = "Zac Efron", prof = "Actor")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+//        elevation = 5.dp
+    ) {
+        Box() {
             Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Column(
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .background(Color.Red)
-                        .fillMaxHeight(0.5f),
-                    verticalArrangement = Arrangement.Bottom,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column (
+                    modifier = Modifier.padding(start = 16.dp)
                 ) {
-                    Text(text = "hello")
-                    Text(text = "Error")
-                    Text(text = "Trum-trum")
-                }
-                Column(
-                    modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "hello")
-                    Text(text = "Error")
-                    Text(text = "Trum-trum")
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
