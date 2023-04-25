0e0ab61058a8e2ddf19ca677dcd40b95c944cb9a
Evgeniy Shakhov <79115839983@ya.ru>
2023-04-25 14:40:03 +0300

    lesson 2

--- app/src/main/java/com/eshakhov/jetpack1/MainActivity.kt
+++ app/src/main/java/com/eshakhov/jetpack1/MainActivity.kt
@@ -3,12 +3,21 @@ package com.eshakhov.jetpack1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.eshakhov.jetpack1.ui.theme.JetpackTheme
 
@@ -16,31 +25,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .fillMaxHeight(0.5f),
+                    verticalArrangement = Arrangement.Bottom,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Text(text = "hello")
+                    Text(text = "Error")
+                    Text(text = "Trum-trum")
+                }
+                Column(
+                    modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "hello")
+                    Text(text = "Error")
+                    Text(text = "Trum-trum")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackTheme {
-        Greeting("Android")
     }
 }
