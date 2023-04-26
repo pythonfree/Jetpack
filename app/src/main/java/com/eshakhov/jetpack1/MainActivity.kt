b42f468f3f4e8b6b7c814836e6dc9ebf609ffd69
Evgeniy Shakhov <79115839983@ya.ru>
2023-04-26 09:04:26 +0300

    lesson 4

--- app/src/main/java/com/eshakhov/jetpack1/MainActivity.kt
+++ app/src/main/java/com/eshakhov/jetpack1/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.eshakhov.jetpack1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,8 +20,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
@@ -28,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,11 +46,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
-                ListItem(name = "Zac Efron", prof = "Actor")
-                ListItem(name = "Zac Efron", prof = "Actor")
-                ListItem(name = "Zac Efron", prof = "Actor")
-                ListItem(name = "Zac Efron", prof = "Actor")
+            Column(
+                modifier = Modifier.verticalScroll(rememberScrollState())
+            ) {
+                ListItem(name = "Zac Efron1", prof = "Actor")
+                ListItem(name = "Zac Efron2", prof = "Actor")
+                ListItem(name = "Zac Efron3", prof = "Actor")
+                ListItem(name = "Zac Efron4", prof = "Actor")
+                ListItem(name = "Zac Efron5", prof = "Actor")
+                ListItem(name = "Zac Efron6", prof = "Actor")
+                ListItem(name = "Zac Efron7", prof = "Actor")
+                ListItem(name = "Zac Efron8", prof = "Actor")
+                ListItem(name = "Zac Efron9", prof = "Actor")
+                ListItem(name = "Zac Efron0", prof = "Actor")
+                ListItem(name = "Zac Efron-", prof = "Actor")
+                ListItem(name = "Zac Efron=", prof = "Actor")
+                ListItem(name = "Zac Efronq", prof = "Actor")
+                ListItem(name = "Zac Efronw", prof = "Actor")
+                ListItem(name = "Zac Efrone", prof = "Actor")
+                ListItem(name = "Zac Efronr", prof = "Actor")
+                ListItem(name = "Zac Efront", prof = "Actor")
+                ListItem(name = "Zac Efrony", prof = "Actor")
+                ListItem(name = "Zac Efronu", prof = "Actor")
+                ListItem(name = "Zac Efroni", prof = "Actor")
+                ListItem(name = "Zac Efrono", prof = "Actor")
+                ListItem(name = "Zac Efronp", prof = "Actor")
+                ListItem(name = "Zac Efron[", prof = "Actor")
             }
         }
     }
@@ -53,7 +82,22 @@ private fun ListItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .clickable {
+                Log.d("tag1", "Clicked")
+            }
+            .pointerInput(Unit) {
+//                detectDragGesturesAfterLongPress { change, dragAmount ->
+//                    Log.d("tag2", "Long pressed: $dragAmount")
+//                    Log.d("tag3", "Long pressed: ${change.isConsumed}")
+//                }
+//                detectTapGestures {
+//                    Log.d("tag4", "detectTapGestures: $it")
+//                }
+                detectHorizontalDragGestures { change, dragAmount ->
+                    Log.d("tag5", "detectHorizontalDragGestures: $dragAmount")
+                }
+            },
         shape = RoundedCornerShape(15.dp),
 //        elevation = 5.dp
     ) {
